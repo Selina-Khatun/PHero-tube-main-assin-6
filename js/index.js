fad031d58e7ab1b28e184d87bd9543bf023b7f2b
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2023-09-01 17:52:37 +0600

    tab section done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// console.log("hello")
+const allCategory = async () => {
+    const response = await fetch(
+        " https://openapi.programming-hero.com/api/videos/categories"
+    );
+    const data = await response.json();
+
+    // const allData=data.data;
+    // console.log(allData);
+    const tabContainer = document.getElementById("tab-container")
+    data.data.forEach((category) => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+       <button class="btn px-5 normal-case">${category.category}</button>
+       `;
+        // if (category.index === 0) {
+        //     div.classList.add("active");
+        // }
+        tabContainer.appendChild(div);
+    });
+};
+allCategory();
